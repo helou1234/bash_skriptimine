5c0612ac6cea9f58baabccc4ebcbba986b75bdd5
Helena Õunapuu <helena.ounapuu@khk.ee>
2020-12-16 12:04:43 +0200

    kaustade varundamine /home kaustast

--- backup.sh
+++ backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#    See skript varundab automaatselt arhiivina kasutajate kodukaustad root kausta home_bcp ja lisab varundamis kuupäeva
-
+#    See skript varundab automaatselt arhiivina /home kaustad root kausta home_bcp
 kuupaev=`date +%d.%m.%Y`
+sihtkaust="/home"
 varunduskoht="/home/home_bcp/"
-for kasutaja in `more ./kasutajad.txt`; do
-tar -cf $varunduskoht$kasutaja[$kuupaev].tar home/$kasutaja
-#loon käsu, mis varundab kõikide  kasutajate kodukaustad, kes on mainitud kasutajad.txt failis
-done
+if [ -d $sihtkaust ]; then
+	for kataloog in $(ls $sihtkaust); do #annan käsu, et iga leitud kaust arhiveerida ja kokku pakkida
+		tar -cf $varunduskoht$kataloog[$kuupaev].tar home/$kataloog #teen kaustadest arhiivi
+		gzip $varunduskoht$kataloog[$kuupaev].tar  #pakin arhiivi kokku
+	done
+else
+	echo "Kontrollige, kas sisestasite kausta asukoha ja nime õigesti!"
+	fi
